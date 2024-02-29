d98e5d6fa7d5b1d19e2bc7c4ec24428010c720b9
S.M. Mahmudul Hasan Rudra <71984328+Mahmudul-Rudra@users.noreply.github.com>
2024-03-01 00:53:52 +0600

    image added in the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Heading, Image } from '@chakra-ui/react';
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
@@ -8,6 +8,16 @@ import img3 from '../assets/3.jpg';
 import img4 from '../assets/4.jpg';
 import img5 from '../assets/5.png';
 
+const headingOptions = {
+  pos: 'absolute',
+  left: '50%',
+  top: '50%',
+  transform: 'translate(-50%, -50%)',
+  textTransforms: 'uppercase',
+  p: '4',
+  size: '2xl',
+};
+
 const Home = () => {
   return (
     <Box>
@@ -16,17 +26,40 @@ const Home = () => {
   );
 };
 
-const MyCarousel = () => {
+const MyCarousel = () => (
   <Carousel
     autoPlay
     infiniteLoop
-    interval={1000}
+    interval={3000}
     showStatus={false}
     showThumbs={false}
     showArrows={false}
   >
-    <Box w={'full'}></Box>
-  </Carousel>;
-};
+    <Box w={'full'} h={'100vh'}>
+      <Image src={img1} />
+      <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
+        Watch The Future
+      </Heading>
+    </Box>
+    <Box w={'full'} h={'100vh'}>
+      <Image src={img2} />
+      <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
+        Future Is Gaming
+      </Heading>
+    </Box>
+    <Box w={'full'} h={'100vh'}>
+      <Image src={img3} />
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+        Gaming on Console
+      </Heading>
+    </Box>
+    <Box w={'full'} h={'100vh'}>
+      <Image src={img4} />
+      <Heading bgColor={'blackAlpha.500'} color={'white'} {...headingOptions}>
+        Night Life is Cool
+      </Heading>
+    </Box>
+  </Carousel>
+);
 
 export default Home;
