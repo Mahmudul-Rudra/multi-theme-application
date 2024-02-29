2e1a5b6a57aba6c05994db67c038f4bfb91429c0
S.M. Mahmudul Hasan Rudra <71984328+Mahmudul-Rudra@users.noreply.github.com>
2024-02-29 23:49:35 +0600

    creating drawer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,62 @@
 import React from 'react';
+import {
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerCloseButton,
+  Button,
+  useDisclosure,
+  VStack,
+} from '@chakra-ui/react';
 
+import { Link } from 'react-router-dom';
+import { BiMenuAltLeft } from 'react-icons/bi';
 const Header = () => {
-  return <div>Header</div>;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Button
+        pos={'fixed'}
+        top={'4'}
+        left={'2'}
+        colorScheme="purple"
+        p={'0'}
+        w={'10'}
+        h={'10'}
+        borderRadius={'full'}
+        onClick={onOpen}
+      >
+        <BiMenuAltLeft size={'20'} />
+      </Button>
+
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Video Hub</DrawerHeader>
+          <DrawerBody>
+            <VStack>
+              <Button variant={'ghost'} colorScheme="purple">
+                <Link to={'/'}>Home</Link>
+              </Button>
+              <Button variant={'ghost'} colorScheme="purple">
+                <Link to={'/videos'}>Videos</Link>
+              </Button>
+              <Button variant={'ghost'} colorScheme="purple">
+                <Link to={'/videos?category'}>Free Videos</Link>
+              </Button>
+              <Button variant={'ghost'} colorScheme="purple">
+                <Link to={'/upload'}>Upload Videos</Link>
+              </Button>
+            </VStack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
 };
 
 export default Header;
