cdc00eb231bb01c7edf789559df5a66d1a7037e8
S.M. Mahmudul Hasan Rudra <71984328+Mahmudul-Rudra@users.noreply.github.com>
2024-03-01 09:56:48 +0600

    home page done and footer done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Image } from '@chakra-ui/react';
+import { Box, Container, Heading, Image, Stack, Text } from '@chakra-ui/react';
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
@@ -22,6 +22,45 @@ const Home = () => {
   return (
     <Box>
       <MyCarousel />
+      <Container minH={'100vh'} maxW={'container.xl'} p={'16'}>
+        <Heading
+          textTransform={'uppercase'}
+          py={'2'}
+          w={'fit-content'}
+          borderBottom={'2px solid'}
+          m={'auto'}
+        >
+          Services
+        </Heading>
+        <Stack
+          h="full"
+          p={'4'}
+          alignItem={'center'}
+          direction={['column', 'row']}
+        >
+          <Image src={img5} filter={'hue-rotate(-150deg)'} h={['40', '400']} />
+          <Text
+            lineHeight={'180%'}
+            letterSpacing={'widest'}
+            p={['4', '16']}
+            textAlign={'center'}
+          >
+            Est irure id dolore incididunt Lorem aute in cupidatat non fugiat
+            velit exercitation velit. Duis do esse laborum do velit dolor tempor
+            enim non adipisicing irure consequat incididunt. Excepteur occaecat
+            nostrud consectetur et quis. Incididunt veniam ex amet exercitation
+            laboris velit. Consectetur minim voluptate reprehenderit in tempor
+            irure sit dolore nostrud sunt esse nulla esse. Nisi cillum anim id
+            enim cupidatat cillum et incididunt quis. Esse velit amet
+            exercitation mollit commodo. Lorem laboris consequat in qui officia
+            nostrud cupidatat excepteur irure ullamco consectetur deserunt
+            aliqua. Esse non fugiat laboris reprehenderit in sit labore
+            adipisicing mollit elit Lorem consectetur. Duis qui officia sint
+            ullamco ullamco enim id aliquip sint voluptate aliquip exercitation
+            ipsum.
+          </Text>
+        </Stack>
+      </Container>
     </Box>
   );
 };
@@ -35,27 +74,32 @@ const MyCarousel = () => (
     showThumbs={false}
     showArrows={false}
   >
-    <Box w={'full'} h={'100vh'}>
+    <Box w="full" h={'100vh'}>
       <Image src={img1} />
       <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
         Watch The Future
       </Heading>
     </Box>
-    <Box w={'full'} h={'100vh'}>
+    <Box w="full" h={'100vh'}>
       <Image src={img2} />
       <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
         Future Is Gaming
       </Heading>
     </Box>
-    <Box w={'full'} h={'100vh'}>
+    <Box w="full" h={'100vh'}>
       <Image src={img3} />
       <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
         Gaming on Console
       </Heading>
     </Box>
-    <Box w={'full'} h={'100vh'}>
+    <Box w="full" h={'100vh'}>
       <Image src={img4} />
-      <Heading bgColor={'blackAlpha.500'} color={'white'} {...headingOptions}>
+      <Heading
+        bgColor={'blackAlpha.500'}
+        color={'white'}
+        {...headingOptions}
+        m={'0'}
+      >
         Night Life is Cool
       </Heading>
     </Box>
